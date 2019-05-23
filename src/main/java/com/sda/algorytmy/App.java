91139c21f80577d980f8959f50c7d6fde158df0b
ciechann <emgasuk@gmail.com>
2019-05-23 22:30:29 +0100

    final

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,10 +1,22 @@
 package com.sda.algorytmy;
 
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Podaj pierwszy wyraz ciągu: ");
+        int a = scan.nextInt();
+        System.out.println("Podaj stały przyrost; ");
+        int d = scan.nextInt();
+        System.out.println("Podaj numer wyrazu do wygenerowania: ");
+        int n = scan.nextInt();
 
+       for(int i=a; i<=n; i++){
+           System.out.print(a + (i - 1)*d);
+       }
     }
 }
